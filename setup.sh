ef132d429267b7684b05d82200ea3317bc89a4c0
Maxx Lehmann <mglehman@mtu.edu>
2017-09-17 14:35:05 +0200

    Inconsolata gets installed

--- setup.sh
+++ setup.sh
@@ -10,5 +10,22 @@ ln -s dots/vimrc .vimrc
 rm .bashrc
 ln -s dots/aliases.sh .bashrc
 
+
+# Font stuff
+if [[ `uname` == 'Darwin' ]]; then
+  font_dir="$HOME/Library/Fonts"
+else
+  font_dir="$HOME/.local/share/fonts"
+  mkdir -p $font_dir
+fi
+
+echo "Installing Patched Inconsolata"
+cp dots/fonts/inconsolata-powerline.otf "$font_dir/"
+
+echo "Resetting font cache, this may take a moment..."
+fc-cache -f $font_dir
+"Powerline Inconsolata installed to $font_dir"
+
+
 vim +PluginInstall +qall
 
