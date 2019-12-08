af2f0420a668ecd30099801fb46951b164b5f25d
Maxx Lehmann <mglehman@mtu.edu>
2019-12-08 14:35:41 -0600

    add browser-sync alias

--- aliases.sh
+++ aliases.sh
@@ -19,3 +19,6 @@ alias l="git log --graph -5 --decorate --all --abbrev-commit --pretty=format:'%C
 alias log="git log --all --stat --oneline -3 --graph --abbrev-commit --pretty=format:'%C(dim normal)%<(12)%an -%Creset %C(yellow)%h%Creset: %C(dim cyan)%<(14)<%cr> %Creset%s %Cred%d%Creset%n'"
 
 alias d='git diff'
+
+alias serve='browser-sync start --server --files \"css/*.css, js/*.js, *.html\" --no-notify'
+
