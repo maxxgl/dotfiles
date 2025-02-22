c594337ab4dabcda6d191fccfb585f7f4b732eda
Maxx Lehmann <mglehman@mtu.edu>
2025-02-22 13:59:37 -0700

    add arch stuff

---
+++ arch/arch-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv ~/.config/hypr ~/.config/old-hypr
+
+ln -s ~/dotfiles/hypr ~/.config/hypr
+ln -s ~/dotfiles/waybar ~/.config/waybar
---
+++ arch/packages.sh
@@ -0,0 +1,7 @@
+sudo pacman -S tmux ripgrep fd zip fzf lua lazygit nodejs npm ruff python firefox wl-clipboard
+
+npm install -g neovim
+
+ENV TERM="xterm-256color"
+
+EXPOSE 80 3000 5173 8000 8080
